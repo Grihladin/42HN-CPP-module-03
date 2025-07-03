b4477171f9a42e4c197404cbbb4af512a1e3e52c
mratke <mratke@student.42heilbronn.de>
2025-07-03 17:31:24 +0200

    From now on i will write normal commit messeges. ex00: added more tests for clarity that it works

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,15 +6,19 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 21:46:37 by mratke            #+#    #+#             */
-/*   Updated: 2025/07/02 22:08:47 by mratke           ###   ########.fr       */
+/*   Updated: 2025/07/03 17:29:48 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ClapTrap.hpp"
 
 int main() {
-  ClapTrap a("Attacker");
+  ClapTrap a("Bob");
   a.attack("YOU");
   a.takeDamage(5);
+  a.takeDamage(4);
+  a.beRepaired(3);
+  a.beRepaired(3);
+  a.beRepaired(3);
   a.beRepaired(3);
 }
