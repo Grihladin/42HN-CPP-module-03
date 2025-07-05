99b933b3cdcc530a5f4a26f7d6f65e30cbbb94ed
mratke <mratke@student.42heilbronn.de>
2025-07-05 19:03:34 +0200

    ex01: added tests

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,15 +6,13 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 21:46:37 by mratke            #+#    #+#             */
-/*   Updated: 2025/07/02 22:08:47 by mratke           ###   ########.fr       */
+/*   Updated: 2025/07/05 19:03:00 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/ClapTrap.hpp"
+#include "../inc/ScavTrap.hpp"
 
 int main() {
-  ClapTrap a("Attacker");
-  a.attack("YOU");
-  a.takeDamage(5);
-  a.beRepaired(3);
+  ScavTrap g("gosha");
+  g.attack("YOU");
 }
